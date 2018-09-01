f4081693a87b5fcd0dffc1deadbd5ac868d46c2b
Aliou Diallo <code@aliou.me>
2018-09-01 12:26:08 +0200

    Make sure the DateTime.Range types are correct.

--- lib/date_time/range.ex
+++ lib/date_time/range.ex
@@ -11,12 +11,31 @@ defmodule DateTime.Range do
         }
 
   @spec new(DateTime.t(), DateTime.t(), boolean(), boolean()) :: t
-  @doc "Create a new DateTime Range"
-  def new(lower, upper, lower_inclusive \\ true, upper_inclusive \\ true),
-    do: %__MODULE__{
+  @doc ~S"""
+  Create a new DateTime Range
+
+  ## Examples
+
+      iex> DateTime.Range.new(DateTime.from_unix!(0), DateTime.from_unix!(1))
+      %DateTime.Range{
+        lower: DateTime.from_unix!(0),
+        lower_inclusive: true,
+        upper: DateTime.from_unix!(1),
+        upper_inclusive: true
+      }
+
+  """
+  def new(lower, upper, lower_inclusive \\ true, upper_inclusive \\ true)
+
+  def new(%DateTime{} = lower, %DateTime{} = upper, lower_inclusive, upper_inclusive)
+      when is_boolean(lower_inclusive) and is_boolean(upper_inclusive) do
+    %__MODULE__{
       lower: lower,
       upper: upper,
       lower_inclusive: lower_inclusive,
       upper_inclusive: upper_inclusive
     }
+  end
 end
+
+# TODO: defimpl Inspect, for: DateTime.Range
---
+++ test/date_time/range_test.exs
@@ -0,0 +1,4 @@
+defmodule DateTime.RangeTest do
+  use ExUnit.Case, async: true
+  doctest DateTime.Range
+end
