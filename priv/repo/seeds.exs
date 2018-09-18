a53a31a6aeacb3f2bb7d856400587ddf5ef5f18e
Aliou Diallo <code@aliou.me>
2018-09-18 16:39:49 +0200

    Create ships in seed file.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Radch.{
+  Repo,
+  Ship
+}
+
+Repo.insert!(%Ship{
+  name: "Justice of Toren",
+  ship_class: "Justice"
+})
+
+Repo.insert!(%Ship{
+  name: "Mercy of Kalr",
+  ship_class: "Mercy"
+})
+
+Repo.insert!(%Ship{
+  name: "Sword of Amaat",
+  ship_class: "Sword"
+})
